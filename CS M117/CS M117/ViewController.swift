c7631f0bf377c7d205ce0c79014881afa83fad86
shreyk98 <shreyk98@g.ucla.edu>
2018-05-07 21:21:01 -0700

    basic login page added

--- CS M117/CS M117/ViewController.swift
+++ CS M117/CS M117/ViewController.swift
@@ -7,9 +7,61 @@
 //
 
 import UIKit
-
+import Firebase
+import FirebaseAuth
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    @IBAction func action(_ sender: UIButton)
+    {
+        if emailText.text != "" && passwordText.text != ""
+        {
+            if segmentControl.selectedSegmentIndex == 0         // login
+            {
+                Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!, completion: {(user, error) in
+                    if user != nil          //user exists, and sign in successful
+                    {
+                        print("Sucess")
+                    }
+                    else                    // user not available, going to print error messages
+                    {
+                        if let myError = error?.localizedDescription        // generic error messages, if available
+                        {
+                            print (myError)
+                        }
+                        else                // no generic messages error, so print my message
+                        {
+                            print ("Error")
+                        }
+                    }
+                })
+            }
+            else                                                // sign up user
+            {
+                Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!, completion: {(user, error) in
+                    if user != nil          //user exists, and sign up successful
+                    {
+                        print("Sucess")
+                    }
+                    else                    // user not available, going to print error messages
+                    {
+                        if let myError = error?.localizedDescription        // generic error messages, if available
+                        {
+                            print (myError)
+                        }
+                        else                // no generic messages error, so print my message
+                        {
+                            print ("Error")
+                        }
+                    }
+                })
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
