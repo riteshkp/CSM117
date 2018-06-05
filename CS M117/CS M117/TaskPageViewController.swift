7a0a9ec98ec4fc355511c185ad001307b850c349
Priya <priyakanipakam@Priyas-MacBook-Pro.local>
2018-06-05 09:56:22 -0700

    fixed infinite printing bug

--- CS M117/CS M117/TaskPageViewController.swift
+++ CS M117/CS M117/TaskPageViewController.swift
@@ -22,7 +22,7 @@ class TaskPageViewController: UIViewController, UITableViewDelegate, UITableView
     override func viewDidLoad() {
         fetchTasks()
     }
-
+    
     @IBAction func plus(_ sender: Any) {
         self.performSegue(withIdentifier: "addTask", sender: self)
     }
@@ -43,77 +43,59 @@ class TaskPageViewController: UIViewController, UITableViewDelegate, UITableView
         let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath) as! TaskCell
         cell.configureCell(tasks: loadedTasks[indexPath.row])
         /*if tasks[indexPath.row].isCompleted{//Sets checkbox accordingly
-            cell.checkBoxOutlet.setBackgroundImage(#imageLiteral(resourceName: "checkBoxFILLED-1"), for: UIControlState.normal)
-        } else {
-            cell.checkBoxOutlet.setBackgroundImage(#imageLiteral(resourceName: "checkBoxOUTLINE "), for: UIControlState.normal)
-        }*/
+         cell.checkBoxOutlet.setBackgroundImage(#imageLiteral(resourceName: "checkBoxFILLED-1"), for: UIControlState.normal)
+         } else {
+         cell.checkBoxOutlet.setBackgroundImage(#imageLiteral(resourceName: "checkBoxOUTLINE "), for: UIControlState.normal)
+         }*/
         cell.delegate = self
         cell.indexP = indexPath.row
         cell.loadedTasks = loadedTasks
         return cell
     }
     
+    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    //        let vc = segue.destination as! ViewController
+    //        let vc = segue.destination as! AddTaskViewController
+    //        vc.delegate = self
+    //    }
+    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
     {
         if editingStyle == .delete
         {
+            
+            let addpoints: Int? = self.loadedTasks[indexPath.row].points //points to give to user
+            
             let userRef = dataBaseRef.child("USERS/\(Auth.auth().currentUser!.uid)")    //get user info
-            userRef.observe(.value) { (snapshot) in
-                
+            userRef.observeSingleEvent(of: .value, with: { (snapshot) in
                 let user = User(snapshot: snapshot)
                 let userPoints = user.points! //get user points
-                let addpoints: Int? = self.loadedTasks[indexPath.row].points
-                let newpoints = userPoints + addpoints!; //add points from this task
-                
-               // print(tasks.(indexPath).points)
-              //  print(indexPath.row)
-              //  print( self.tasks[indexPath.row] )
-                
-              //  userRef = Database.database().reference()
-                //user.ref.child("Points").setValue(newpoints)
-                //child("Points").setValue(newpoints)
-                user.ref.updateChildValues(["Points":newpoints]) //changes user points in database
-                
-                
-                //remove in database
-                //print(self.loadedTasks[indexPath.row].ref)
-                //let ref = self.dataBaseRef.child("Tasks").child(self.loadedTasks[indexPath.row].ref).removeValue()
-                
-                self.loadedTasks[indexPath.row].ref.removeValue() //remove task from database
-                self.loadedTasks.remove(at: indexPath.row) //remove task from tableview
-                
-                tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.fade)
-              //  tableView.reloadData()
-
+                let newpoints = userPoints + addpoints!;
+                user.ref.child("Points").setValue(newpoints)
+            }) { (error) in
+                print(error.localizedDescription)
             }
             
-           
-            //remove task from database
+            
+            self.loadedTasks[indexPath.row].ref.updateChildValues(["user":Auth.auth().currentUser!.uid]) //add which user completed the task to the db
+            
+            let count = self.tableView(self.tableView, numberOfRowsInSection: 0) //this clears the tableview
+            self.loadedTasks.removeAll()
+            self.tableView.deleteRows(at: (0..<count).map({ (i) in IndexPath(row: i, section: 0)}), with: .automatic)
             
         }
     }
     
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        let vc = segue.destination as! ViewController
-//        let vc = segue.destination as! AddTaskViewController
-//        vc.delegate = self
-//    }
     
     func addTask(name: String, points: Int, deadline: Date) {
         tasks.append(Task(taskID: name, taskPoints: points, deadline: deadline))
         tableView.reloadData()
     }
     
-//    func changeButton(checked: Bool, index: Int?) {
-//        tasks[index!].isCompleted = checked
-//        tableView.reloadData()
-//    }
-    
     func changeButton(checked: Bool, index: Int?) {
-        
+        //        tasks[index!].isCompleted = checked
+        //        tableView.reloadData()
     }
-    
-    
     
     func fetchTasks() {
         let userRef = dataBaseRef.child("USERS/\(Auth.auth().currentUser!.uid)")    //get user info
@@ -132,7 +114,7 @@ class TaskPageViewController: UIViewController, UITableViewDelegate, UITableView
                 }
                 
                 for tasks in results {
-                    if tasks.Group == userGroup {
+                    if tasks.Group == userGroup && tasks.user == ""{
                         self.loadedTasks.append(tasks)
                     }
                 }
@@ -142,7 +124,5 @@ class TaskPageViewController: UIViewController, UITableViewDelegate, UITableView
             })
         }
     }
-
 }
-
 
