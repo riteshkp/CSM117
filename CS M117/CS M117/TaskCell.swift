2a6504b5467fd524146aa03d8cb290287b447e2b
Ritesh Pendekanti <riteshkp@hotmail.com>
2018-06-06 19:54:16 -0700

    .

--- CS M117/CS M117/TaskCell.swift
+++ CS M117/CS M117/TaskCell.swift
@@ -23,8 +23,34 @@ class TaskCell: UITableViewCell {
     
     func configureCell(tasks: DatabaseTask){
         self.taskNameLabel.text = tasks.key
+        self.pointsLabel.text = String(tasks.points)
+        self.deadlineLabel.text = tasks.deadline
+        
+        let currentDateTime = Date()
+        
+        if tasks.deadline < dateToString(givenDate: currentDateTime){
+            self.taskNameLabel.textColor = UIColor.red
+            self.pointsLabel.textColor = UIColor.red
+            self.deadlineLabel.textColor = UIColor.red
+        }
+        else {
+            self.taskNameLabel.textColor = UIColor.black
+            self.pointsLabel.textColor = UIColor.black
+            self.deadlineLabel.textColor = UIColor.black
+        }
     }
     
+    func dateToString(givenDate: Date) -> String
+    {
+        let date = givenDate // Get Todays Date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        let stringDate: String = dateFormatter.string(from: date as! Date)
+        return stringDate
+    }
+    
+    @IBOutlet weak var deadlineLabel: UILabel!
+    @IBOutlet weak var pointsLabel: UILabel!
     @IBOutlet weak var checkBoxOutlet: UIButton!
     @IBOutlet weak var taskNameLabel: UILabel!
     var delegate: ChangeButton?
